6a6fcb9bd8664776bc69e99d16e13043e2ef4e80
Josh Lieberman <josh.lieberman92@gmail.com>
2014-09-19 13:26:57 -0400

    initial commit of xcode project.  added paddle and ball to xib.

---
+++ Breakout-iOS/Breakout-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Breakout-iOS
+//
+//  Created by Josh L on 9/19/14.
+//  Copyright (c) 2014 Applico Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
