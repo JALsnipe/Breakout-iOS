7ed3f2378f19a773afec5892a6a56d4a32eb9db1
Josh Lieberman <josh.lieberman92@gmail.com>
2014-09-22 10:56:19 -0400

    pushing work from Friday

--- Breakout-iOS/Breakout-iOS/ViewController.m
+++ Breakout-iOS/Breakout-iOS/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollisionBehaviorDelegate>
 @property (weak, nonatomic) IBOutlet UIView *ball;
 @property (weak, nonatomic) IBOutlet UIView *paddle;
 @property (weak, nonatomic) IBOutlet UIView *brick1;
@@ -26,6 +26,7 @@ UIDynamicAnimator *_animator;
 UIGravityBehavior *_gravity;
 UICollisionBehavior *_collision;
 UIDynamicItemBehavior *bounceBehaviour;
+UIPushBehavior *pusher;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -34,7 +35,9 @@ UIDynamicItemBehavior *bounceBehaviour;
     _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     _gravity = [[UIGravityBehavior alloc] initWithItems:@[_ball]];
     [_animator addBehavior:_gravity];
+    
     _collision = [[UICollisionBehavior alloc] initWithItems:@[_ball, _brick1, _brick2, _brick3, _brick4, _brick5]];
+    _collision.collisionDelegate = self;
     _collision.translatesReferenceBoundsIntoBoundary = YES;
     [_animator addBehavior:_collision];
     
@@ -46,17 +49,29 @@ UIDynamicItemBehavior *bounceBehaviour;
                                   toPoint:rightEdge];
     
     // Bounce!
-    bounceBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[ ]];
-    bounceBehaviour.elasticity = 1.1;
+    bounceBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[_ball]];
+    bounceBehaviour.elasticity = 1.0;
+//    UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[_ball]];
+//    [itemBehavior addAngularVelocity:M_PI_2 forItem:_ball];
+//    [_animator addBehavior:itemBehavior];
     [_animator addBehavior:bounceBehaviour];
-    
-    [bounceBehaviour addItem:_ball];
     
     // paddle movement
     UIPanGestureRecognizer* pgr = [[UIPanGestureRecognizer alloc]
                                    initWithTarget:self
                                    action:@selector(handlePan:)];
     [_paddle addGestureRecognizer:pgr];
+    
+    // push
+//    pusher = [[UIPushBehavior alloc] initWithItems:@[_ball] mode:UIPushBehaviorModeContinuous];
+//    pusher.pushDirection = CGVectorMake(0.5, 1.0);
+//    pusher.active = YES;
+//    [_animator addBehavior:pusher];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    _gravity = [[UIGravityBehavior alloc] initWithItems:@[_ball]];
+    [_animator addBehavior:_gravity];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -82,6 +97,33 @@ UIDynamicItemBehavior *bounceBehaviour;
                                     fromPoint:_paddle.frame.origin
                                       toPoint:rightEdge];
     }
+}
+- (void)collisionBehavior:(UICollisionBehavior *)behavior
+      beganContactForItem:(id<UIDynamicItem>)item1
+                 withItem:(id<UIDynamicItem>)item2
+                  atPoint:(CGPoint)p {
+    
+    pusher.active = NO;
+    
+    NSLog(@"item 1: %@, item 2: %@", item1, item2);
+    
+    UIView *view1 = (UIView *)item1;
+    NSLog(@"%ld", (long)view1.tag);
+    
+    UIView *view2 = (UIView *)item2;
+    NSLog(@"%ld", (long)view2.tag);
+    
+    if(view2.tag == 1) {
+        [view2 removeFromSuperview];
+    }
+}
+
+- (void)collisionBehavior:(UICollisionBehavior*)paramBehavior
+      beganContactForItem:(id <UIDynamicItem>)paramItem
+   withBoundaryIdentifier:(id <NSCopying>)paramIdentifier
+                  atPoint:(CGPoint)paramPoint{
+    NSLog(@"paramItem: %@, paramIdentifier: %@", paramBehavior, paramIdentifier);
+    pusher.active = NO;
 }
 
 @end
